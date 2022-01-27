d7c98a8efdb01ba300816833fff7c38ca079d305
Lucas Moraes <lucas_moraes2011@outlook.com>
2022-01-27 14:56:32 -0300

    feat: changing user photo

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import { useRouter } from 'next/router';
+import React from 'react';
 import appConfig from '../config.json';
 
-//Componente React
-//Função recebe um argumento/propriedade
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
-  
-
 function Titulo(props){
-    console.log(props);
     const Tag = props.tag;
     return (
         <>
@@ -50,24 +19,15 @@ function Titulo(props){
     );
 }
 
-// function HomePage() {
-//     //JSX
-//     return (
-//         <div>
-//             <GlobalStyle /> 
-//             <Titulo tag="h2">Bem-vindos de volta!</Titulo>
-//             <h2>Discord Alura - Spider</h2>
-//         </div>
-//     )
-// } 
-// export default HomePage
-
 export default function PaginaInicial() {
-    const username = 'luquinhasmoraes';
+    // let username = 'luquinhasmoraes';
+
+    const [username, setUsername] = React.useState('luquinhasmoraes')
+    const roteamento = useRouter()
+    const defaultImage = './img/user.jpg'
   
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -86,51 +46,11 @@ export default function PaginaInicial() {
               },
               width: '100%', maxWidth: '700px',
               borderRadius: '5px', padding: '32px', margin: '16px',
-              boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+              boxShadow: '0 5px 10px 0 rgb(0 0 0 / 20%)',
               backgroundColor: appConfig.theme.colors.transparente.fundo,
-              border: '2px solid #fff'
+              border: '3px solid #29333D'
             }}
           >
-            {/* Formulário */}
-            <Box
-              as="form"
-              styleSheet={{
-                display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-                width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
-              }}
-            >
-              <Titulo tag="h2">Olá, seja bem-vindo!</Titulo>
-              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
-                {appConfig.name}
-              </Text>
-  
-              <TextField
-                fullWidth
-                placeholder='Usuário'
-                textFieldColors={{
-                  neutral: {
-                    textColor: appConfig.theme.colors.neutrals[200],
-                    mainColor: appConfig.theme.colors.neutrals[900],
-                    mainColorHighlight: appConfig.theme.colors.primary[900],
-                    backgroundColor: appConfig.theme.colors.neutrals[800],
-                  },
-                }}
-              />
-              <Button
-                type='submit'
-                label='Entrar'
-                fullWidth
-                buttonColors={{
-                  contrastColor: appConfig.theme.colors.neutrals["000"],
-                  mainColor: appConfig.theme.colors.primary[900],
-                  mainColorLight: appConfig.theme.colors.primary[400],
-                  mainColorStrong: appConfig.theme.colors.primary[600],
-                }}
-              />
-            </Box>
-            {/* Formulário */}
-  
-  
             {/* Photo Area */}
             <Box
               styleSheet={{
@@ -148,7 +68,10 @@ export default function PaginaInicial() {
                   borderRadius: '50%',
                   marginBottom: '16px',
                 }}
-                src={`https://github.com/${username}.png`}
+                onError={function (event) {
+                  event.target.src = defaultImage
+                }}
+                src={ username.length > 2 ? `https://github.com/${username}.png` : defaultImage}
               />
               <Text
                 variant="body4"
@@ -162,6 +85,54 @@ export default function PaginaInicial() {
               </Text>
             </Box>
             {/* Photo Area */}
+
+            {/* Formulário */}
+            <Box
+              as="form"
+              onSubmit={ function (event) {
+                event.preventDefault();
+                roteamento.push('/chat')
+              }}
+              styleSheet={{
+                display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+              }}
+            >
+              <Titulo tag="h2">Olá, seja bem-vindo!</Titulo>
+              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+                {appConfig.name}
+              </Text>
+  
+              <TextField
+                fullWidth
+                placeholder='Usuário'
+                onChange={function (event) {
+                  var usuario = event.target.value
+                  setUsername(usuario)                 
+                }}
+                textFieldColors={{
+                  neutral: {
+                    textColor: appConfig.theme.colors.neutrals[200],
+                    mainColor: appConfig.theme.colors.neutrals[900],
+                    mainColorHighlight: appConfig.theme.colors.primary[900],
+                    backgroundColor: appConfig.theme.colors.transparente.fundo,
+                  },
+                }}
+              />
+              <Button
+                type='submit'
+                label='Entrar'
+                fullWidth
+                buttonColors={{
+                  contrastColor: appConfig.theme.colors.neutrals["000"],
+                  mainColor: appConfig.theme.colors.primary[900],
+                  mainColorLight: appConfig.theme.colors.primary[400],
+                  mainColorStrong: appConfig.theme.colors.primary[600],
+                }}
+              />
+            </Box>
+            {/* Formulário */}
+  
           </Box>
         </Box>
       </>
